36ca010fb3917592fdbeb079c1a572081b0c6ec5
AGCol1 <agcolclough1@gmail.com>
2025-02-14 15:11:37 +0000

    index and css changes

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ function submitCode() {
     var code = document.getElementById("code").value;
     if (code === "1234") {
         window.location.href = "https://agcol.uk/school_projects.html";
-    } else if (code === "5678") {
-        window.location.href = "https://agcol.uk/personal_projects.html";
-    }else {
+    } else {
         alert("Invalid code. Please try again.");
     }
     console.log("Entered code: " + code);
