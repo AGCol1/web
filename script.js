60ac9f7228fa2cb2971dbf70008cf56faacab70f
AGCol1 <agcolclough1@gmail.com>
2025-02-14 10:00:46 +0000

    personal projects page added

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ function submitCode() {
     var code = document.getElementById("code").value;
     if (code === "1234") {
         window.location.href = "https://agcol.uk/school_projects.html";
-    } else {
+    } else if (code === "5678") {
+        window.location.href = "https://agcol.uk/personal_projects.html";
+    }else {
         alert("Invalid code. Please try again.");
     }
     console.log("Entered code: " + code);
