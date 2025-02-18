c7ee062a69d5b43eb01bc687a789b5a8349ed932
AGCol1 <agcolclough1@gmail.com>
2025-02-18 04:29:56 +0000

    js fixes

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
 function submitCode() {
     var code = document.getElementById("code").value;
     if (code === "1234") {
-        window.location.href = "https://agcol.uk/school_projects.html";
-    } else {
-        alert("Invalid code. Please try again.");
-    }
-    console.log("Entered code: " + code);
-}
-
-function submitCode() {
-    var code = document.getElementById("code").value;
-    if (code === "1234") {
-        window.location.href = "https://agcol.uk/school_projects.html";
+        window.location.href = "https://agcol.uk/school_projects";
     } else {
         alert("Invalid code. Please try again.");
     }
