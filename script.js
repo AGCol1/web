e932588fa771717dd592e485c8fae67418cb924a
AGCol1 <agcolclough1@gmail.com>
2025-05-13 21:30:48 +0100

    script remove

--- script.js
+++ script.js
@@ -2,17 +2,6 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
-function submitCode() {
-    var code = document.getElementById("code").value;
-    if (code === "1234") {
-        alert("Code accepted. Click Ok to view...");
-        window.location.href = "https://agcol.uk/school_projects";
-    } else {
-        alert("Invalid code. Please try again.");
-    }
-    console.log("Entered code: " + code);
-}
-
 document.addEventListener("DOMContentLoaded", function() {
     const text = "ALFIE COLCLOUGH";
     let index = 0;
